c13322639f02012deef2e6e22fca9e9a7b2f4fe6
Michael <michaelhv@uia.no>
2020-08-31 20:25:03 +0200

    Discord app, lacking functionality

---
+++ MHVbot/src/bot.js
@@ -0,0 +1,176 @@
+require('dotenv').config();
+
+const { Client, WebhookClient } = require('discord.js');
+const { prefix, token } = ('./config.json');
+const ytdl = require('ytdl-core');
+
+const client = new Client();
+
+const webhookClient = new WebhookClient(
+  process.env.WEBHOOK_ID,
+  process.env.WEBHOOK_TOKEN,
+);
+
+const queue = new Map();
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in.`)
+} )
+
+client.on("message", async message => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+  
+    const serverQueue = queue.get(message.guild.id);
+  
+    if (message.content.startsWith(`${prefix}play`)) {
+      execute(message, serverQueue);
+      return;
+    } else if (message.content.startsWith(`${prefix}skip`)) {
+      skip(message, serverQueue);
+      return;
+    } else if (message.content.startsWith(`${prefix}stop`)) {
+      stop(message, serverQueue);
+      return;
+    } else {
+      message.channel.send("You need to enter a valid command!");
+    }
+  });
+  
+  async function execute(message, serverQueue) {
+    const args = message.content.split(" ");
+  
+    const voiceChannel = message.member.voice.channel;
+    if (!voiceChannel)
+      return message.channel.send(
+        "You need to be in a voice channel to play music!"
+      );
+    const permissions = voiceChannel.permissionsFor(message.client.user);
+    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+      return message.channel.send(
+        "I need the permissions to join and speak in your voice channel!"
+      );
+    }
+  
+    const songInfo = await ytdl.getInfo(args[1]);
+    const song = {
+      title: songInfo.title,
+      url: songInfo.video_url
+    };
+  
+    if (!serverQueue) {
+      const queueContruct = {
+        textChannel: message.channel,
+        voiceChannel: voiceChannel,
+        connection: null,
+        songs: [],
+        volume: 5,
+        playing: true
+      };
+  
+      queue.set(message.guild.id, queueContruct);
+  
+      queueContruct.songs.push(song);
+  
+      try {
+        var connection = await voiceChannel.join();
+        queueContruct.connection = connection;
+        play(message.guild, queueContruct.songs[0]);
+      } catch (err) {
+        console.log(err);
+        queue.delete(message.guild.id);
+        return message.channel.send(err);
+      }
+    } else {
+      serverQueue.songs.push(song);
+      return message.channel.send(`${song.title} has been added to the queue!`);
+    }
+  }
+  
+  function skip(message, serverQueue) {
+    if (!message.member.voice.channel)
+      return message.channel.send(
+        "You have to be in a voice channel to stop the music!"
+      );
+    if (!serverQueue)
+      return message.channel.send("There is no song that I could skip!");
+    serverQueue.connection.dispatcher.end();
+  }
+  
+  function stop(message, serverQueue) {
+    if (!message.member.voice.channel)
+      return message.channel.send(
+        "You have to be in a voice channel to stop the music!"
+      );
+    serverQueue.songs = [];
+    serverQueue.connection.dispatcher.end();
+  }
+  
+  function play(guild, song) {
+    const serverQueue = queue.get(guild.id);
+    if (!song) {
+      serverQueue.voiceChannel.leave();
+      queue.delete(guild.id);
+      return;
+    }
+  
+    const dispatcher = serverQueue.connection
+      .play(ytdl(song.url))
+      .on("finish", () => {
+        serverQueue.songs.shift();
+        play(guild, serverQueue.songs[0]);
+      })
+      .on("error", error => console.error(error));
+    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+    serverQueue.textChannel.send(`Start playing: **${song.title}**`);
+  }
+
+client.on('message', (message) => {
+    if(message.author.bot) return;
+    console.log(message.content)
+    if (message.content === '!commands') {
+        message.reply('Currently only got: !github, !drive and !time')
+    }
+} )
+
+client.on('message', (message) => {
+    if(message.author.bot) return;
+    console.log(message.content)
+    if (message.content === '!drive') {
+        message.reply('Here is the link to Google Drive: https://drive.google.com/drive/folders/1fc6lX0shQYXnCEe0ssKR9LbtlsYno39m')
+    }
+} )
+
+client.on('message', (message) => {
+    if(message.author.bot) return;
+    console.log(message.content)
+    if (message.content === '!time') {
+        message.reply('Here is the link to time table: https://docs.google.com/spreadsheets/d/1hd7wNzm5uL4NAMoMBJwvhIV7t1mcNGCpeBJDPqzTMOw/edit#gid=0')
+    }
+} )
+
+client.on('message', (message) => {
+    if(message.author.bot) return;
+    console.log(message.content)
+    if (message.content === '!github') {
+        message.reply('Here is the link to the Github repository: https://github.com/OriginalMHV/Gruppe3')
+    }
+} )
+
+client.on('message', (message) => {
+  if(message.author.bot) return;
+  console.log(message.content)
+  if (message.content === '!timeedit') {
+      message.reply('Here is the link to the schedule: https://cloud.timeedit.net/uia/web/tp/ri106v5y6566Z6QY85Q8QYgXZQ0360Y9727.html')
+  }
+} )
+
+client.on('message', (message) => {
+    if(message.author.bot) return;
+    console.log(message.content)
+    if (message.content === 'Who is the best?') {
+        message.reply('is the best')
+    }
+} )
+
+client.login(process.env.DISCORDJS_BOT_TOKEN)
